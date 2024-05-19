d40cdc3961b43e5bdcbf14b96647c4270c080bdf
YONGHEE <ayh0429@gmail.com>
2024-05-19 16:47:27 +0900

    add env

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDDq9Kgw4nCg7CzBh18A9Jc8d0Qpg--MYo",
-  authDomain: "kiosk-ordering.firebaseapp.com",
-  projectId: "kiosk-ordering",
-  storageBucket: "kiosk-ordering.appspot.com",
-  messagingSenderId: "1011439035578",
-  appId: "1:1011439035578:web:102548e3d41a644b3ace83",
-  measurementId: "G-ZKQT9J25PR",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
